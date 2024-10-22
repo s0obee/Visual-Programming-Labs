11f21edad38bd13ddb7d3c1e0979dcda8d4ca736
Yunus Emre Çakıcı <yunuscakici.08@gmail.com>
2024-10-22 15:05:16 +0300

    5.HaftaLab
    Butona basıldığında, 100 ms içinde formun sınırları içinde kalacak şekilde butonun yeri rastgele değiştirilir. Kullanıcı 60 saniye içinde butona tekrar basıp butonun konumunun değişimini iptal etmesi lazım. 60 saniyeyi progress bar’da da gösterdim. Eğer 60 saniye içinde butona basıp durduramazsa kullanıcıya “Game is over” mesajını göstereceğiz. Eğer durdurabilirse “Congratulations”  mesajı gösterilir.

---
+++ Lab001/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Lab001
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnClick = new System.Windows.Forms.Button();
+            this.lblRemainingTime = new System.Windows.Forms.Label();
+            this.progressBarTimer = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.lblRemainingTime2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnClick
+            // 
+            this.btnClick.BackColor = System.Drawing.Color.Cyan;
+            this.btnClick.Location = new System.Drawing.Point(295, 76);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(141, 94);
+            this.btnClick.TabIndex = 0;
+            this.btnClick.Text = "Click";
+            this.btnClick.UseVisualStyleBackColor = false;
+            this.btnClick.Click += new System.EventHandler(this.btnClick_Click);
+            // 
+            // lblRemainingTime
+            // 
+            this.lblRemainingTime.AutoSize = true;
+            this.lblRemainingTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblRemainingTime.Location = new System.Drawing.Point(198, 279);
+            this.lblRemainingTime.Name = "lblRemainingTime";
+            this.lblRemainingTime.Size = new System.Drawing.Size(188, 25);
+            this.lblRemainingTime.TabIndex = 1;
+            this.lblRemainingTime.Text = "RemainingTime :";
+            // 
+            // progressBarTimer
+            // 
+            this.progressBarTimer.Location = new System.Drawing.Point(-8, 410);
+            this.progressBarTimer.Name = "progressBarTimer";
+            this.progressBarTimer.Size = new System.Drawing.Size(815, 49);
+            this.progressBarTimer.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // lblRemainingTime2
+            // 
+            this.lblRemainingTime2.AutoSize = true;
+            this.lblRemainingTime2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblRemainingTime2.ForeColor = System.Drawing.Color.Red;
+            this.lblRemainingTime2.Location = new System.Drawing.Point(431, 279);
+            this.lblRemainingTime2.Name = "lblRemainingTime2";
+            this.lblRemainingTime2.Size = new System.Drawing.Size(38, 25);
+            this.lblRemainingTime2.TabIndex = 3;
+            this.lblRemainingTime2.Text = "60";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblRemainingTime2);
+            this.Controls.Add(this.progressBarTimer);
+            this.Controls.Add(this.lblRemainingTime);
+            this.Controls.Add(this.btnClick);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClick;
+        private System.Windows.Forms.Label lblRemainingTime;
+        private System.Windows.Forms.ProgressBar progressBarTimer;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label lblRemainingTime2;
+    }
+}
+
---
+++ Lab001/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab001
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int counter = 60;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblRemainingTime2.Text = counter.ToString();
+            progressBarTimer.Step = 1;
+            progressBarTimer.Maximum = counter;
+            progressBarTimer.Minimum = 0;
+            progressBarTimer.Value = 0;
+            timer2.Interval = 1000;
+        }
+
+        private void btnClick_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Stop();
+                timer2.Stop();
+                if(counter > 0)
+                {
+                    MessageBox.Show("Oyunu Kazandınız ..: ");
+                }
+            }
+            else
+            {
+                timer1.Start();
+                timer2.Start();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random rmd = new Random();
+            btnClick.Location = new Point(rmd.Next(0,Size.Width), rmd.Next(0,Size.Height));
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            counter--;
+            lblRemainingTime2.Text = counter.ToString();
+            progressBarTimer.PerformStep();
+            if (counter <= 0)
+            {
+                timer1.Stop();
+                timer2.Stop();
+                MessageBox.Show("Oyun Bitti. Kaybettiniz ..: ");
+            }
+        }
+    }
+}
